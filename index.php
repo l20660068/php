5f8fad581e6852a221a0842d1f1d57789ced6acb
l20660068 <l20660068@matehuala.tecnm.mx>
2024-10-14 07:34:35 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+// Datos
+$nombre = "Diego Méndez Puente";
+$numero_control = "20660068";
+$fecha_actual = date("d-m-Y");
+
+// Imprimir mensaje
+echo "Hola Mundo, mi nombre es $nombre, mi número de control es $numero_control y la fecha de hoy es $fecha_actual.";
+?>
